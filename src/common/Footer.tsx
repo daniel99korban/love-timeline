00967a4821121d1c58d5839e0bf32507da5021b9
Davi Komura de Castro <davi.komura@gmail.com>
2025-03-25 21:06:13 +0000

    Small change to the footer

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -5,20 +5,23 @@ export const Footer = () => {
     const { t } = useTranslation();
 
     return (
-        <footer className="bg-gradient-to-r from-gray-800 to-gray-900 text-gray-300 py-4 fixed bottom-0 left-0 w-full z-50">
-            <div className="container mx-auto px-4 flex flex-col sm:flex-row items-center justify-center sm:justify-between">
+        <>
+            <footer className="bg-gradient-to-r from-gray-800 to-gray-900 text-gray-300 py-4 fixed bottom-0 left-0 w-full z-50">
+                <div className="container mx-auto px-4 flex flex-col sm:flex-row items-center justify-between">
                 <p className="text-sm">
-                {t('footer.copyright')}
+                    {t('footer.copyright')}
                 </p>
                 <div className="mt-2 sm:mt-0 flex space-x-4">
-                <a href="#" className="text-sm hover:underline">
+                    <a href="#" className="text-sm hover:underline">
                     {t('footer.privacy_policy')}
-                </a>
-                <a href="#" className="text-sm hover:underline">
+                    </a>
+                    <a href="#" className="text-sm hover:underline">
                     {t('footer.terms_of_use')}
-                </a>
+                    </a>
                 </div>
-            </div>
-        </footer>
+                </div>
+            </footer>
+            <div className="h-16"></div>
+        </>
     )
 }
