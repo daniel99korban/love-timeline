58524af4db6066266748f1b6d91c7b4285f51c96
Davi Komura de Castro <davi.komura@gmail.com>
2025-04-15 16:07:20 +0100

    Correcoes de texto e rotas

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,14 +1,17 @@
-import { LanguageDropdown } from '../components/LanguageDropdown';
+import { LanguageDropdown } from "../components/LanguageDropdown";
+import { Link } from "react-router";
 
 export const Header = () => {
   return (
     <header className=" w-full bg-gray-950 border-t border-gray-800 text-gray-400 ">
       <div className="h-16 flex items-center justify-between px-6 max-w-screen-xl mx-auto">
-        <img
-          src="../../../public/logos/logobranco.png"
-          alt="Love Timeline"
-          className="h-8 object-contain"
-        />
+        <Link to="/">
+          <img
+            src="/logos/logobranco.png"
+            alt="Love Timeline"
+            className="h-8 object-contain"
+          />
+        </Link>
         <LanguageDropdown />
       </div>
     </header>
